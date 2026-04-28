562c256871c0dfa86c3c014c3c751823c639ace2
durbek-saydaliyev <durbek@emotivonline.com>
2026-04-28 10:51:33 +0500

    chore: update sha256 checksum for orbit v0.1.0 release

--- Formula/orbit.rb
+++ Formula/orbit.rb
@@ -2,7 +2,7 @@ class Orbit < Formula
   desc "Memory observability and safe optimizer CLI"
   homepage "https://github.com/durbek-saydaliyev/orbit"
   url "https://github.com/durbek-saydaliyev/orbit/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "c4fe48b1675a881f6a7008122d330c4e2cdf2daea94eaa8c8e512a318358c276"
 
   depends_on "go" => :build
 
